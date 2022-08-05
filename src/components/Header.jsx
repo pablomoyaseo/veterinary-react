6714cb9b9216b6e9272ab1e2fc06a52655e93387
unknown <you@example.com>
2022-08-05 17:52:43 -0400

    Cambio de header a git hub

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header () {
     
     return (
        
-        <h1 className="text-4xl uppercase font-black text-center md:w-1/2 mx-auto ">Seguimiento Pacientes <span className="text-indigo-600">Veterinaria</span></h1>
+        <h1 className="text-4xl uppercase font-black text-center md:w-1/2 mx-auto ">Seguimiento Pacientes {""} Con Github {""} <span className="text-indigo-600">Veterinaria</span></h1>
         
     )
 }
